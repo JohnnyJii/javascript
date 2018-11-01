c91f364282368bc53ed1da1e89c2c95bd6b770ae
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2018-11-01 12:44:03 +0200

    Lab 5-1

--- untitled4/Lab 5/DOM-tehtavat-master/js/teht1.js
+++ untitled4/Lab 5/DOM-tehtavat-master/js/teht1.js
@@ -1,2 +1,24 @@
-const erkki = document.getElementsByTagName('tr')[1];
-erkki.getElementsByTagName('td')[0].innerHTML = 'Jukka';
+document.getElementsByTagName('td')[0].innerHTML = 'Jukka';
+document.getElementsByTagName('td')[1].innerHTML = 'Juntunen';
+document.getElementsByTagName('td')[2].innerHTML = 'Johnny';
+document.getElementsByTagName('td')[3].innerHTML = 'Saunalahdenkatu, 02330 Espoo';
+
+const tr1 = document.querySelector('tr:first-child');
+const th = document.createElement('th');
+const p = document.createTextNode('Puhelinro: ');
+th.appendChild(p);
+tr1.appendChild(th);
+
+const tr2 = document.querySelector('tr:last-child');
+const td =document.createElement('td');
+const p2 = document.createTextNode('0401640060');
+td.appendChild(p2);
+tr2.appendChild(td);
+
+let parent = document.getElementsByTagName('tr');
+let node = document.getElementsByTagName('th')[2];
+parent[0].removeChild(node);
+
+let node2 = document.getElementsByTagName('td')[2];
+parent[1].removeChild(node2);
+
