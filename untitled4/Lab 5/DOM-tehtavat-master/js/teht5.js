b1a042f134aa6a61afe3204e12fd1f8bb4e7cbce
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2018-11-05 10:37:17 +0200

    Lab 5-3

--- untitled4/Lab 5/DOM-tehtavat-master/js/teht5.js
+++ untitled4/Lab 5/DOM-tehtavat-master/js/teht5.js
@@ -1,17 +1,13 @@
 function updateClock() {
-  var now = new Date(), // current date
+  var now = new Date(),
       months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'
         , 'September', 'October', 'November', 'December'];
   kello = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(),
 
-      date = [now.getDate(),
-        months[now.getMonth()],
-        now.getFullYear()].join(' ');
+      date = [now.getDate(), months[now.getMonth()], now.getFullYear()].join(' ');
 
-  // set the content of the element with the ID time to the formatted string
   document.getElementById('kello').innerHTML = [date, kello].join(' / ');
 
-  // call this function again in 1000ms
-  setTimeout(updateClock, 1000);
+    setTimeout(updateClock, 1000);
 }
-updateClock(); // initial call
+updateClock();
